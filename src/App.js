ed697dcff3456137798318c1f44a319ea23d5200
syamjir <rsyamjir@gmail.com>
2024-11-05 21:28:24 +0530

    Add main App component with routing setup and context provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import FlashRatePage from "./pages/FlashRatePage";
+import RateAlertPage from "./pages/RateAlertPage";
+import CurrencyPage from "./pages/CurrencyPage";
+import Loader from "./components/Loader";
+import CurrencyCardPage from "./pages/CurrencyCardPage";
+import CurrencyInfoPage from "./pages/CurrencyInfoPage";
+import NotFound from "./pages/NotFound";
+import { CurrencyDataProvider } from "./contexts/CurrencyContext";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const ConversionPage = lazy(() => import("./pages/ConversionPage"));
+const ChartPage = lazy(() => import("./pages/ChartPage"));
+const FeaturePage = lazy(() => import("./pages/FeaturePage"));
 
 function App() {
-  return <div className="text-sky-400">TEST</div>;
+  return (
+    <div className="h-full flex justify-center items-center pb-9">
+      <CurrencyDataProvider>
+        <BrowserRouter>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route index element={<HomePage />} />
+              <Route path="conversion" element={<ConversionPage />} />
+              <Route path="chart" element={<ChartPage />} />
+              <Route path="features" element={<FeaturePage />}>
+                <Route index element={<Navigate replace to="flash-rate" />} />
+                <Route path="flash-rate" element={<FlashRatePage />} />
+                <Route path="rate-alert" element={<RateAlertPage />} />
+                <Route path="currency-details" element={<CurrencyPage />}>
+                  <Route
+                    index
+                    element={<Navigate replace to="currency-cards" />}
+                  />
+                  <Route path="currency-cards" element={<CurrencyCardPage />} />
+                  <Route
+                    path="currency-cards/:code"
+                    element={<CurrencyInfoPage />}
+                  />
+                </Route>
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </CurrencyDataProvider>
+    </div>
+  );
 }
 
 export default App;
